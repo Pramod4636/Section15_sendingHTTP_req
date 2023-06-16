b92c39dd775c292560ec78cde41515880984f181
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-16 14:34:11 +0530

    loading state : when data is loading show loadding... and when comoleted then show component movieList  using state vaible isLoading

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [dummyMovies,setMovies] = useState([]);
-  
+  const [movies,setMovies] = useState([]);
+  const [isLoading ,setIsLoading]  = useState(false);
+
+
 async function fetchMoviesHandler (){
 
+  setIsLoading(true);
   const response = await fetch('https://swapi.dev/api/films') ; 
   const data = await response.json();
 
@@ -19,7 +22,9 @@ async function fetchMoviesHandler (){
       }
     }) ;
   
-       setMovies(transformedMovies);
+  setMovies(transformedMovies);
+   setIsLoading(false);
+
 }
 
   return (
@@ -28,7 +33,9 @@ async function fetchMoviesHandler (){
         <button onClick={ fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        { !isLoading && movies.length > 0  && <MoviesList movies={movies} /> } 
+        { isLoading && <p> Loading ...... </p> } 
+        {!isLoading && movies.length === 0 && <p> No movies found </p> }
       </section>
     </React.Fragment>
   );
