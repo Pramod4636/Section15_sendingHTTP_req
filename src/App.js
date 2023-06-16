2a98e15c8dc937924c3e25bf646f99bd040d1026
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-16 14:20:49 +0530

    sending HTTP request using async function and await

--- src/App.js
+++ src/App.js
@@ -6,25 +6,26 @@ import './App.css';
 function App() {
   const [dummyMovies,setMovies] = useState([]);
   
-fetch('https://swapi.dev/api/films')
-  .then( (response) => {return response.json();} )
-  .then( (data) => {
-    const transformedMovies = data.results.map( movieData => {
+async function fetchMoviesHandler (){
+
+  const response = await fetch('https://swapi.dev/api/films') ; 
+  const data = await response.json();
+
+  const transformedMovies = data.results.map( movieData => {
       return {id : movieData.episode_id ,
           title : movieData.title ,
           openingText : movieData.opening_crawl,
           releaseDate : movieData.release_data
       }
     }) ;
-
-    setMovies(transformedMovies);
-  }); 
-
   
+       setMovies(transformedMovies);
+}
+
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={ fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
         <MoviesList movies={dummyMovies} />
