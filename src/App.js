1fe4a95f71447459c2f6ec2e21385a88279e43b5
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-16 14:15:47 +0530

    fetch api send the requeast

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import './App.css';
 function App() {
   const [dummyMovies,setMovies] = useState([]);
   
+fetch('https://swapi.dev/api/films')
+  .then( (response) => {return response.json();} )
+  .then( (data) => {
+    const transformedMovies = data.results.map( movieData => {
+      return {id : movieData.episode_id ,
+          title : movieData.title ,
+          openingText : movieData.opening_crawl,
+          releaseDate : movieData.release_data
+      }
+    }) ;
 
+    setMovies(transformedMovies);
+  }); 
+
+  
   return (
     <React.Fragment>
       <section>
